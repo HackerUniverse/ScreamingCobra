e084841013f8fb5eb36174d8f23f2c3f4d8cda58
Hacker_Universe <41442211+HackerUniverse@users.noreply.github.com>
2019-08-03 04:58:19 -0700

    new interface with termux support
    added two menus

--- interface.sh
+++ interface.sh
@@ -10,19 +10,43 @@ function banner() {
 echo "
 Screaming Cobra Loader
                 version 1.0a
+                Script by:Hackeruniversee
 "
 }
 banner
-echo -e "$red [$green+$red] 1:$off Normal Version ";
-echo -e "$red [$green+$red] 2:$off Dom Case Version ";
-echo -ne "$red [$green+$red] Selectg An Option:$off: " ;
+echo -e "$red [$green+$red] 1:$off Linux Users"; 
+echo -e "$red [$green+$red] 2:$off Termux Users";
+echo -ne "$red [$green+$red] Select An Option:$off: " ;
 read Option
 if [ $Option -eq "1" ]
 then
-    ./ScreamingCobra1.py
-fi
+ echo -e "$red [$green+$red] 1:$off Normal Version "; 
+ echo -e "$red [$green+$red] 2:$off Dom Case Version ";
+ echo -ne "$red [$green+$red] Selectg An Option:$off: " ;
+ read Option
+ if [ $Option -eq "1" ]
+ then
+    python ScreamingCobra1.py
+ fi
 
+ if [ $Option -eq "2" ]
+ then
+    python ScreamingCobra2.py
+ fi
+fi 
 if [ $Option -eq "2" ]
 then
-    ./ScreamingCobra2.py
-fi
+ echo -e "$red [$green+$red] 1:$off Normal Version ";
+ echo -e "$red [$green+$red] 2:$off Dom Case Version ";
+ echo -ne "$red [$green+$red] Selectg An Option:$off: " ;
+ read Option
+ if [ $Option -eq "1" ]
+ then
+    python2 ScreamingCobra1.py
+ fi
+
+ if [ $Option -eq "2" ]
+ then
+    python2 ScreamingCobra2.py
+ fi
+fi 
